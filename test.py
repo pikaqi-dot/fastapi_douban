e6ce470f7ddc9fb5a7009b20692a2ac5e90843ec
pikaqi-dot <66110001+pikaqi-dot@users.noreply.github.com>
2023-03-05 17:23:54 +0000

    hbdscm

--- test.py
+++ test.py
@@ -36,15 +36,15 @@ def getTop250(url):
 def movie(url):
   time.sleep(0.5)
   global datalist
-  h = getHtml(url)
-  datalist['movie']['lists'] = h.html.xpath(
+  h1 = getHtml(url)
+  datalist['movie']['lists'] = h1.html.xpath(
     '//*[@id="db-nav-movie"]/div[2]/div/ul/li/a/text()')
   datalist['movie']['overview'] = {}
   datalist['movie']['overview']['hoting'] = {}
-  datalist['movie']['overview']['hoting']['title'] = h.html.xpath(
+  datalist['movie']['overview']['hoting']['title'] = h1.html.xpath(
     '//*[@id="screening"]/div[1]/h2/text()', first=True)
   datalist['movie']['overview']['hoting']['content'] = []
-  for item in h.html.xpath(
+  for item in h1.html.xpath(
       '//*[@class="screening-bd"]//*[@class="ui-slide-item"]')[:-2]:
     data = {}
     f=item.find('img',first=True)
@@ -57,16 +57,26 @@ def movie(url):
     data['imgSrc'] = f.attrs['src']
     data['rating'] = item.find('.subject-rate', first=True).text
     detailUrl = item.find('.poster>a', first=True).attrs['href']
-    h = getHtml(detailUrl)
-    data['director'] = h.html.find('#info .attrs a', first=True).text
-    data['actor'] = h.html.xpath('//*[@id="info"]/span[3]/span[2]/a/text()')
-    ratingNum=h.html.find('.rating_people>span')
+    h2 = getHtml(detailUrl)
+    data['director'] = h2.html.find('#info .attrs a', first=True).text
+    data['actor'] = h2.html.xpath('//*[@id="info"]/span[3]/span[2]/a/text()')
+    ratingNum=h2.html.find('.rating_people>span')
     data['ratingNum']=(len(ratingNum) and ratingNum[0].text) or "无"
-    data['date']=h.html.xpath('//*[@id="content"]/h1/span[2]/text()',first=True)
-    movie_time=h.html.xpath('//*[@id="info"]/*[@property="v:runtime"]/text()')
+    data['date']=h2.html.xpath('//*[@id="content"]/h1/span[2]/text()',first=True)
+    movie_time=h2.html.xpath('//*[@id="info"]/*[@property="v:runtime"]/text()')
     data['time']=(len(movie_time) and movie_time[0]) or "暂无此信息"
-    data['area']=re.findall("制片国家/地区: (.*?)',",str( h.html.find('.article #info')[0].text.split('\n')))[0] or "暂无此信息"
+    data['area']=re.findall("制片国家/地区: (.*?)',",str( h2.html.find('.article #info')[0].text.split('\n')))[0] or "暂无此信息"
     datalist['movie']['overview']['hoting']['content'].append(data)
+  datalist['movie']['overview']['hoting']['pinglun']=[]
+  for item in h1.html.xpath('//*[@id="reviews"]/div[2]/div'):
+    data={}
+    data['imgSrc']=item.find('img')[0].attrs['src']
+    data['title']=item.find('.review-bd>h3>a')[0].text
+    data['name']=item.find('.review-bd>.review-meta>a:nth-child(1)')[0].text
+    data['movieName']=item.find('.review-bd>.review-meta>a:nth-child(2)')[0].text
+    data['star']=item.find('.review-bd>.review-meta>span')[0].search('<span class="allstar{}"/>')[0]
+    datalist['movie']['overview']['hoting']['pinglun'].append(data)
+    
     
 
 
--- xixi.py
+++ xixi.py
@@ -2,11 +2,8 @@ from requests_html import HTMLSession, UserAgent
 import re,json
 
 session = HTMLSession()
-h = session.get('https://movie.douban.com/subject/10876425/?from=playing_poster',
+h = session.get('https://movie.douban.com',
                 headers={'User-Agent': UserAgent().random})
      
-print(
-
-    re.findall("制片国家/地区: (.*?)',",str( h.html.find('.article #info')[0].text.split('\n')))
-  
- )
+for item in h.html.xpath('//*[@id="reviews"]/div[2]/div'):
+  print()
